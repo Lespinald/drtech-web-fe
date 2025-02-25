58d2b523e161557eb1496a140e43b417a0831dd4
Lespinald <55605978+Lespinald@users.noreply.github.com>
2025-02-24 22:19:48 -0500

    first commit

---
+++ script.tsx
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.querySelector(".boton");
+    boton.addEventListener("click", function () {
+        alert("¡Has hecho clic en el botón!");
+    });
+});
